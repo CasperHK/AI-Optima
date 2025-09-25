84fd460f87c5a13c3086a04f725d439e9a77feb2
Casper LI <casperli@hko.hksarg>
2025-09-25 14:21:09 +0800

    Update frontend.

---
+++ src/frontend/content/config.ts
@@ -0,0 +1,6 @@
+import { defineCollection } from "@nuxt/content";
+
+export default defineCollection({
+  // Example: add your collection configuration here
+  // See https://content.nuxt.com/docs/api/collection for options
+});
